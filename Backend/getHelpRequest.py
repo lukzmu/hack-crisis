260b876bf191d59ac23e47688558ae9ad8a49039
Jacek B <sigint89@gmail.com>
2020-03-19 20:50:44 +0000

    Updated getHelpRequest.py, final and working

--- Backend/getHelpRequest.py
+++ Backend/getHelpRequest.py
@@ -1,18 +1,23 @@
 import json
 import boto3
-
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('HelpRequest')
-
+deviceId = ''
 def lambda_handler(event, context):
-    response = table.get_item(
-        Key={
-            'deviceId': 'testID2'
-            }
-    )
-    
-    print(response)
-    return {
-        'statusCode': 200,
-        'body': response
-    }
+    print(event)
+    deviceId = event['queryStringParameters']['deviceId']
+    try:   
+        response = table.get_item(
+            Key={
+                'deviceId': deviceId
+                }
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response['Item']),
+        }
+    except:
+        return {
+            'statusCode': 404,
+            'body': '{"message": "Not found."}'
+        }
