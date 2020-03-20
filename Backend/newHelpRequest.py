4562b9b8adef2026eaca11c16f9b486130d343d1
Jacek B <sigint89@gmail.com>
2020-03-20 00:18:19 +0000

    api integrated new HelpRequest function

--- Backend/newHelpRequest.py
+++ Backend/newHelpRequest.py
@@ -4,17 +4,41 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('HelpRequest')
 
+deviceId  = ''
+water  = ''
+food  = ''
+numberOfPeople = ''
+meds = ''
+latitude = ''
+longitude = ''
+isConv = ''
+description = ''
+
 def lambda_handler(event, context):
+    print(event)
+    deviceId  = event['queryStringParameters']['deviceId']
+    water = event['queryStringParameters']['water']
+    food = event['queryStringParameters']['food']
+    numberOfPeople = event['queryStringParameters']['numberOfPeople']
+    meds = event['queryStringParameters']['meds']
+    latitude = event['queryStringParameters']['latitude']
+    longitude = event['queryStringParameters']['longitude']
+    isConv = event['queryStringParameters']['isConv']
+    description = event['queryStringParameters']['description']
+
     response = table.put_item(
         Item={
-            'deviceId': 'testID23',
-            'water': 'Critical',
-            'food': 'Critical',
-            'meds': 'Critical',
-            'isConv': 'True',
-            'latitude': '27',
-            'longitude': '72',
-            'numberOfPeople': '3',
-            'description': 'test2'
+            'deviceId': deviceId,
+            'water': water,
+            'food': food,
+            'numberOfPeople': numberOfPeople,
+            'meds': meds,
+            'latitude': latitude,
+            'longitude': longitude,
+            'isConv': isConv,
+            'description': description
+
+
+            
             }
     )
