645e5c7c3d038d5b8768073b46b8981dead890c0
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2020-03-20 12:32:50 +0100

    update information view layout

--- iOS/CrisisHelp/CrisisHelp/View/InfoView.swift
+++ iOS/CrisisHelp/CrisisHelp/View/InfoView.swift
@@ -11,43 +11,64 @@ import SwiftUI
 struct InfoView: View {
     @ObservedObject var model = InfoViewModel()
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(named: "greenDark")!]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(named: "greenDark")!]
+    }
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack {
-                    VStack(alignment: .leading) {
-                        Text("Coronavirus disease (COVID-19) is a new strain that was discovered in 2019. Common signs of infection include respiratory symptoms, fever, cough, shortness of breath and breathing difficulties. In more severe cases, infection can cause pneumonia, severe acute respiratory syndrome, kidney failure and even death.")
+                    VStack() {
+                        Divider()
+                        HStack {
+                            Text("Keep yourself safe!")
+                            .fontWeight(.bold)
+                            Spacer()
+                        }
+                        .padding()
+                        Text("Coronavirus disease (COVID-19) is a new strain that was discovered in December 2019 and has not been previously identified in humans.")
+                            .font(.footnote)
                             .multilineTextAlignment(.center)
                             .padding()
-                        
-                    }
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.1))
-                            .frame(height: 70)
-                            .padding([.leading, .trailing])
-                        HStack(alignment: .center) {
-                            Text("😮").font(.title).padding(.trailing)
-                            Text("Don't panic!").font(.title)
-                        }
+                        Text("Common signs of infection include respiratory symptoms, fever, cough, shortness of breath and breathing difficulties. In more severe cases, infection can cause pneumonia, severe acute respiratory syndrome, kidney failure and even death.")
+                            .font(.footnote)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                        Text("Standard recommendations to prevent infection spread include regular hand washing, covering mouth and nose when coughing and sneezing, thoroughly cooking meat and eggs. Avoid close contact with anyone showing symptoms of respiratory illness such as coughing and sneezing.")
+                            .font(.footnote)
+                            .multilineTextAlignment(.center)
+                            .padding()
                     }
                     VStack {
-                        Text("Places to check for information:")
-                        .fontWeight(.bold)
+                        Divider()
+                        HStack {
+                            Text("Check for more information")
+                            .fontWeight(.bold)
+                            Spacer()
+                        }
                         .padding()
                         
                         ForEach([String](model.buttonData.keys), id: \.self) { key in
                             Button(action: {
                                 self.model.navigateToWebsite(link: self.model.buttonData[key]!)
                             }) {
-                                Text(key).padding()
+                                Text(key)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color("greenLight"))
+                                    .foregroundColor(Color("greenDark"))
+                                    .cornerRadius(80)
+                                    .padding([.leading, .trailing, .bottom])
                             }
                         }
                     }
-                    .navigationBarTitle("Keep yourself safe!")
+                    .navigationBarTitle("Information")
                     .navigationBarItems(trailing:
                         NavigationLink(destination: HackathonView()) {
                             Image(systemName: "heart.fill")
+                            .foregroundColor(Color("greenDark"))
                         }
                     )
                 }
