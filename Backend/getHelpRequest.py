018ffc0f84a3123f93a360253e0567704d8020c9
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2020-03-20 16:21:45 +0000

    fix function not handling other types than string

--- Backend/getHelpRequest.py
+++ Backend/getHelpRequest.py
@@ -1,23 +1,32 @@
 import json
 import boto3
+from decimal import Decimal
+
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('HelpRequest')
 deviceId = ''
+
 def lambda_handler(event, context):
-    print(event)
     deviceId = event['queryStringParameters']['deviceId']
-    try:   
-        response = table.get_item(
-            Key={
-                'deviceId': deviceId
-                }
-        )
+    response = table.get_item(
+        Key={
+            'deviceId': str(2),
+        }
+    )
+    if 'Item' in response:
         return {
             'statusCode': 200,
-            'body': json.dumps(response['Item']),
+            'body': json.dumps(response['Item'], default=handle_decimal_type),
         }
-    except:
-        return {
-            'statusCode': 404,
-            'body': '{"message": "Not found."}'
-        }
+    return {
+        'statusCode': 404,
+        'body': '{"message": "HelpRequest not found."}'
+    }
+    
+def handle_decimal_type(obj):
+  if isinstance(obj, Decimal):
+      if float(obj).is_integer():
+         return int(obj)
+      else:
+         return float(obj)
+  raise TypeError
